15465244ffa5c52a111f48b4f606e01f07a218ef
verissimohenry <verissimohenry04@gmail.com>
2021-07-22 17:59:56 +0100

    fix linter error

--- src/initStart.js
+++ src/initStart.js
@@ -2,8 +2,8 @@ import { displayNav } from './structure';
 // eslint-disable-next-line import/no-cycle
 import project, { Project, removeProject } from './project';
 // eslint-disable-next-line import/no-cycle
-import { 
-  Todo, createTodo, removeTodo, updateStatus
+import {
+  Todo, createTodo, removeTodo, updateStatus,
 } from './todo';
 
 const container = document.getElementById('content');
