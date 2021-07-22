f301ecd2731ef25968aad34d6f534ef5644cbab6
verissimohenry <verissimohenry04@gmail.com>
2021-07-22 13:12:30 +0100

    fix linter error

--- src/project.js
+++ src/project.js
@@ -1,42 +1,48 @@
 // eslint-disable-next-line import/no-cycle
-import { getProjects } from "./initStart";
+import {
+    getProjects
+} from "./initStart";
 
 class Project {
-  constructor(name) {
-    this.name = name;
-    this.id = name.toLowerCase();
-    this.todos = [];
-  }
+    constructor(name) {
+        this.name = name;
+        this.id = name.toLowerCase();
+        this.todos = [];
+    }
 }
 
 const projectModule = (() => {
-  function createProject(target) {
-    const projects = getProjects();
-    const targetValue = target.value.toLowerCase();
-    const invalidName = projects.find(
-      (element) => element.name.toLowerCase() === targetValue
-    );
-    let newProject;
-    if (target.value !== "" && !invalidName) {
-      newProject = new Project(target.value);
-      projects.push(newProject);
-      localStorage.toDoProjects = JSON.stringify(projects);
+    function createProject(target) {
+        const projects = getProjects();
+        const targetValue = target.value.toLowerCase();
+        const invalidName = projects.find(
+            (element) => element.name.toLowerCase() === targetValue
+        );
+        let newProject;
+        if (target.value !== "" && !invalidName) {
+            newProject = new Project(target.value);
+            projects.push(newProject);
+            localStorage.toDoProjects = JSON.stringify(projects);
+        }
+
+        target.value = "";
+        return newProject || "Could not create a project";
     }
 
-    target.value = "";
-    return newProject || "Could not create a project";
-  }
-
-  return {
-    createProject,
-  };
+    return {
+        createProject,
+    };
 })();
 
 function removeProject(projects, project) {
-  projects = projects.filter((element) => element.id !== project.id);
-  localStorage.toDoProjects = JSON.stringify(projects);
-  return projects;
+    projects = projects.filter((element) => element.id !== project.id);
+    localStorage.toDoProjects = JSON.stringify(projects);
+    return projects;
 }
 
 export default projectModule;
-export { Project, projectModule, removeProject };
+export {
+    Project,
+    projectModule,
+    removeProject
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist',
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [{
+         test: /\.scss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+    },],
     },
-    module: {
-        rules: [{
-            test: /\.scss$/i,
-            use: ['style-loader', 'css-loader', 'sass-loader'],
-        }, ],
-    },
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
+  output: {
+  filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 };
